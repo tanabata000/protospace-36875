c9ef27e5f863a655babe98dfd6212897c6425c2a
tanabata000 <tanabata.kaguya@gmail.com>
2021-09-27 11:11:24 +0900

    prototype詳細ページ修正
    プロトタイプのタイトル名を表示するように変更。プロトタイプのタイトル名の下に「by 投稿者名」を表示。リンクにUser詳細ページを紐づける

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,16 @@
 class CommentsController < ApplicationController
 
   def create
-    # Commentモデルの全レコード情報を取得
+    # Commentモデルの全レコード情報を取得（comment_paramsメソッド使用）
     comment = Comment.create(comment_params)
     if comment.save
     # 保存できた場合、
     redirect_to prototype_path(comment.prototype_id)
     # redirect_to "/prototypes/#{comment.prototype_id}"  # コメントと結びつく画像の詳細画面に遷移する
     else 
-      #コントローラが異なるのでprototypesコントローラのshowアクションの＠変数の内容をcommentsコントローラに再定義
+      # prototypesコントローラのshowアクションのリクエストによりparamsが送られてくる
+      # リクエストにより送られてきたparamsのキー（:prototype_id）に紐づいた値を取得
+      # Prototypeモデルからprototype_idの値が一致したレコードを検索し、@prototypeに代入
       @prototype = Prototype.find(params[:prototype_id])
       @comment = Comment.new
       # prototypesのビューファイルのshowを表示
@@ -19,10 +21,11 @@ class CommentsController < ApplicationController
 
   private
   # ストロングパラメータ設定
-  # prototypeモデルで定義したnameという名前でアクセスできるようになった画像ファイルの保存を許可する実装を行います。
   def comment_params
-    # ルーティングネストによりcommentsコントローラのcreateアクションにprototypesのURIに紐づいたcommentsのURIを生成できる（　/prototypes/:prototype_id/comments(.:format)  comments#create　）
-    # パラメータ設定：対象モデル（comment）対象カラム（:image, :content）ハッシュ結合（user_id：ログイン中のユーザID, prototype_id: パラメータ[現在リンクに表示されている画像（prototype）データid]
+    # ルーティングネストによりcommentsコントローラのcreateアクションにprototypesのURIに紐づいたcommentsのURIを生成できる（ /prototypes/:prototype_id/comments(.:format)  comments#create ）
+    # パラメータ設定：対象モデル（comment）対象カラム（:image, :content）ハッシュ結合（user_id：ログイン中のユーザID, prototype_id: パラメータ[詳細画面からのリクエストのparamsに含まれた画像のid（prototype_id）]
+    # comments#createのURIパターン（ /prototypes/:prototype_id/comments ）。この:prototype_idの中にprototype.idが入っている。
+    # prototype_id: params[:prototype_id]という記述で詳細ページから取得したprototypeテーブルのid情報をcommentsテーブルのprototype_idに送っている（prototype.id = comments.prototype_idとなる）
     params.require(:comment).permit(:image, :content).merge(user_id: current_user.id, prototype_id: params[:prototype_id] ) 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
   #各コントローラへのルーティング設定
   # usersルーティング
   resources :users, only: [:edit, :update, :show]
-  # prototypesルーティング
+  # prototypesルーティング設定。
+  # コメントを投稿する際には、どのプロトタイプに対するコメントなのかをパスから判断できるようにしたい（ルーティングのネストを使用）
+  # ネストを利用するとパスの:〇〇という部分に記述された値を、paramsとして送れる。もし、ネストを利用しなかった場合は、:〇〇のような、パスにパラメーターを含められる部分がない。
+  # @prototype = Prototype.find(params[:id])
   resources :prototypes do
     resources :comments, only: [:index, :create]
   end
